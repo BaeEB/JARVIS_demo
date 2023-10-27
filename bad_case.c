87651084787d7791b1762a19dd4d476a3e8510e8
EUIBIN_BAE <ebbae@suresofttech.com>
2023-10-27 09:46:40 +0900

    Fixed automatically #52 by Vulcan

--- bad_case.c
+++ bad_case.c
@@ -4,10 +4,9 @@ int test0301(int ap, int bp, int cp){
 	int ai = ap;
 	int bi = bp;
 	int ci = cp;
-	int sum = ai
-            + bi // /*    MISRA_C_2012_03_01
+	int sum = ai   /* MISRA_C_2012_03_01 */
+            + bi 
             + ci
-               // */
             ;
     return sum;
 }
