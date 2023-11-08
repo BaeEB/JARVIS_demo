c879d4c5f99d89be17b4cbbb9f715edc3b0c9cf2
EUIBIN_BAE <ebbae@suresofttech.com>
2023-11-08 16:31:05 +0900

    Fixed automatically #99 by JARVIS

--- src/Calculator.c
+++ src/Calculator.c
@@ -1,39 +1,35 @@
 #include <stdio.h>
 #include "Calculator.h"
 
-int Calculator(int a, char c, int b)  
+        
+        int Calculator(int a, char c, int b)  
 {  
     float res;  
-    // printf (" Choose an operator(+, -, *, /) to perform the operation in C Calculator \n ");  
     switch(c)  
     {  
         case '+':  
             res = a + b; // add two numbers  
-            // printf (" Addition of %d and %d is: %.2f", a, b, res);  
             break;  
-          
+           
         case '-':  
             res = a - b; // subtract two numbers  
-            // printf (" Subtraction of %d and %d is: %.2f", a, b, res);  
             break;  
-              
+               
         case '*':  
             res = a * b; // multiply two numbers  
-            // printf (" Multiplication of %d and %d is: %.2f", a, b, res);  
             break;            
-          
+           
         case '/':  
             if (b == 0)   // if b == 0, take another number  
             {  
-                // printf (" \n Divisor cannot be zero. Please enter another value ");  
                 b = 1;
             }  
-            res = a / b; // divide two numbers  
-            // printf (" Division of %d and %d is: %.2f", a, b, res);  
+            res = a / b; // divide two numbers
             break;  
         default:  /* use default to print default message if any condition is not satisfied */  
             printf (" Something is wrong!! Please check the options ");               
     }  
-    int res_int = (int)res;
-    return res;  
+    return (int)res; // return the integer value of res.
 }  
+        
+        
