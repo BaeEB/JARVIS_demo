5da3a1f2b2785ab450793c47bff16fdd73017819
BaeEB <106366276+BaeEB@users.noreply.github.com>
2023-11-08 17:05:45 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
-  ./test 6 0
-  ./test 6 1
-  ./test 6 2
-  ./test 6 3
-  ./test 6 4
-  ./test 6 5
-  ./test 6 6
-  ./test 6 7
-  ./test 6 8
+chmod 777 ./test
+./test 6 0
+./test 6 1
+./test 6 2
+./test 6 3
+./test 6 4
+./test 6 5
+./test 6 6
+./test 6 7
+./test 6 8
