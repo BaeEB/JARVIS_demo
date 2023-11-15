f4795cd00e057f4ee3934a8e81729b4068884fbe
EUIBIN_BAE <ebbae@suresofttech.com>
2023-11-15 17:17:21 +0900

    Fixed automatically #114 by JARVIS

--- bad_case.c
+++ bad_case.c
@@ -1,13 +1,12 @@
 #include "bad_case.h"
 
 int test0301(int ap, int bp, int cp){
-	int ai = ap;
-	int bi = bp;
-	int ci = cp;
-	int sum = ai
-            + bi // /*    MISRA_C_2012_03_01
+    int ai = ap;
+    int bi = bp;
+    int ci = cp;
+    int sum = ai
+            + bi /* sum of ai and bi */
             + ci
-               // */
             ;
     return sum;
 }
