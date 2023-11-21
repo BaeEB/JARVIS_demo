7846a866d677bdfe215f5ea44816f7ee82ea9265
EUIBIN_BAE <ebbae@suresofttech.com>
2023-11-21 17:24:55 +0900

    Fixed automatically #192 by JARVIS

--- bad_case_3.c
+++ bad_case_3.c
@@ -2,9 +2,9 @@
 
 UInt64 test0703(void){
    long a_l = 100L;
-   long b_l = 100l; 
-   UInt64 c_ul = 200L;
-   UInt64 d_ul = 350L;
+   long b_l = 100L; // Corrected: Replaced '100l' with '100L' to comply with MISRA_C_2012_07_03
+   UInt64 c_ul = 200UL; // Corrected: Added 'U' to indicate unsigned long suffix
+   UInt64 d_ul = 350UL; // Corrected: Added 'U' to indicate unsigned long suffix
    UInt64 sum = (UInt64)a_l + (UInt64)b_l + c_ul + d_ul;
    return sum;
 }
