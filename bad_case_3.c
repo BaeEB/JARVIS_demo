a6bd46df23ec51375dec2c85ec1413830b12c2bd
EUIBIN_BAE <ebbae@suresofttech.com>
2023-11-21 11:15:16 +0900

    Fixed automatically #173 by JARVIS

--- bad_case_3.c
+++ bad_case_3.c
@@ -2,9 +2,9 @@
 
 UInt64 test0703(void){
    long a_l = 100L;
-   long b_l = 100l; 
-   UInt64 c_ul = 200L;
-   UInt64 d_ul = 350L;
+   long b_l = 100L; // Corrected suffix for compliance
+   UInt64 c_ul = 200LU; // Added U for unsigned literal and used uppercase L for compliance 
+   UInt64 d_ul = 350LU; // Added U for unsigned literal and used uppercase L for compliance
    UInt64 sum = (UInt64)a_l + (UInt64)b_l + c_ul + d_ul;
    return sum;
 }
