afe5336e4c9ad2466b01a4ca54e08d92db3f1740
EUIBIN_BAE <ebbae@suresofttech.com>
2023-11-21 20:05:29 +0900

    Fixed automatically #196 by JARVIS

--- bad_case_1.c
+++ bad_case_1.c
@@ -1,7 +1,7 @@
 #include "bad_case.h"
 
 short test0902(int x, int y, short e){
-   short buf[ 3 ][ 2 ] = { 1, 2, 0, 0, 5, 6 };
+   short buf[ 3 ][ 2 ] = { { 1, 2 }, { 0, 0 }, { 5, 6 } }; // Corrected to use braces around initializers
    buf[x][y] = e;
    return buf[x][y];
 }
