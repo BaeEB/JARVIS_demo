6bc706c81433febbbada40efffad9667b05f07ee
EUIBIN_BAE <ebbae@suresofttech.com>
2023-11-21 17:30:37 +0900

    Fixed automatically #194 by JARVIS

--- bad_case_2.c
+++ bad_case_2.c
@@ -2,17 +2,18 @@
 
 int test1603(int x)
 {
-	switch(x)
-	{
-	case 1:
-	case 2:
-		x++;
-		break;
-	case 3:	
-		x--;
-	default:
-	    ;
-	}
+    switch(x)
+    {
+    case 1:
+    case 2:
+        x++;
+        break;
+    case 3:  
+        x--;
+        // Fall-through is intentional to default case
+    default:
+        break; // Compliant with MISRA_C_2012_16_03
+    }
 
-	return x;
+    return x;
 }
