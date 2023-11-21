f6ac1acc45227de7e24054370aaf0e680b037762
EUIBIN_BAE <ebbae@suresofttech.com>
2023-11-21 11:30:00 +0900

    Fixed automatically #177 by JARVIS

--- bad_case_1.c
+++ bad_case_1.c
@@ -1,7 +1,7 @@
 #include "bad_case.h"
 
 short test0902(int x, int y, short e){
-   short buf[ 3 ][ 2 ] = { 1, 2, 0, 0, 5, 6 };
+   short buf[ 3 ][ 2 ] = { {1, 2}, {0, 0}, {5, 6} }; // Compliant - Enclosed each sub-object initializer in braces
    buf[x][y] = e;
    return buf[x][y];
 }
