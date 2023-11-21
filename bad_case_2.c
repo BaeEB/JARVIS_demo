61a4a0e25c90b8fb4862b713d96dfb22e56b68ab
EUIBIN_BAE <ebbae@suresofttech.com>
2023-11-21 17:17:45 +0900

    Fixed automatically #190 by JARVIS

--- bad_case_2.c
+++ bad_case_2.c
@@ -2,17 +2,17 @@
 
 int test1603(int x)
 {
-	switch(x)
-	{
-	case 1:
-	case 2:
-		x++;
-		break;
-	case 3:	
-		x--;
-	default:
-	    ;
-	}
-
-	return x;
+    switch(x)
+    {
+    case 1:
+    case 2:
+        x++;
+        break;
+    case 3:    
+        x--;
+        /* fall through */
+    default:
+        break;
+    }
+    return x;
 }
