35be4f4ff34d4de21488f71d0f2cd9f808c7a98d
EUIBIN_BAE <ebbae@suresofttech.com>
2023-11-21 11:36:08 +0900

    Fixed automatically #179 by JARVIS

--- bad_case_3.c
+++ bad_case_3.c
@@ -2,7 +2,7 @@
 
 UInt64 test0703(void){
    long a_l = 100L;
-   long b_l = 100l; 
+   long b_l = 100L; // Corrected: replaced 'l' with 'L'
    UInt64 c_ul = 200L;
    UInt64 d_ul = 350L;
    UInt64 sum = (UInt64)a_l + (UInt64)b_l + c_ul + d_ul;
