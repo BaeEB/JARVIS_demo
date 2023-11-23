bc0275acab1c20c29923f73053ed434be53cef7b
EUIBIN_BAE <ebbae@suresofttech.com>
2023-11-23 14:03:22 +0900

    Fixed automatically #223 by JARVIS

--- src/bad_case_1.c
+++ src/bad_case_1.c
@@ -1,7 +1,7 @@
 #include "bad_case.h"
 
 short test0902(int x, int y, short e){
-   short buf[ 3 ][ 2 ] = { 1, 2, 0, 0, 5, 6 };
+   short buf[ 3 ][ 2 ] = { { 1, 2 }, { 0, 0 }, { 5, 6 } }; // Now compliant with MISRA C:2012 Rule 9.2
    buf[x][y] = e;
    return buf[x][y];
 }
