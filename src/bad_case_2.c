7d4831a9a5913c5d88a0b724f34d42f91fddccf5
EUIBIN_BAE <ebbae@suresofttech.com>
2023-11-30 14:56:11 +0900

    Fixed automatically #270 by JARVIS

--- src/bad_case_2.c
+++ src/bad_case_2.c
@@ -2,17 +2,23 @@
 
 int test1603(int x)
 {
-	switch(x)
-	{
-	case 1:
-	case 2:
-		x++;
-		break;
-	case 3:	
-		x--;
-	default:
-	    ;
-	}
+    switch(x)
+    {
+    case 1:
+    case 2:
+        x++;
+        break;
+    case 3:
+        x--;
+        /* The break statement is required to prevent fall-through from case 3 to default.
+           Since there is an empty default clause, the break statement still needs to be added
+           to comply with MISRA rules even though it doesn't change execution. */
+        break;      
+    default:
+        /* The default clause is empty, no operation here.
+           Included a break for compliance with the MISRA rule. */
+        break;
+    }
 
-	return x;
+    return x;
 }
