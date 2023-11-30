4a11dde32a71db7eb895f6263dff34f9b0d52d6f
EUIBIN_BAE <ebbae@suresofttech.com>
2023-11-30 15:03:13 +0900

    Fixed automatically #272 by JARVIS

--- src/bad_case_2.c
+++ src/bad_case_2.c
@@ -2,17 +2,19 @@
 
 int test1603(int x)
 {
-	switch(x)
-	{
-	case 1:
-	case 2:
-		x++;
-		break;
-	case 3:	
-		x--;
-	default:
-	    ;
-	}
+    switch(x)
+    {
+    case 1:
+    case 2:
+        x++;
+        break;
+    case 3:    
+        x--;
+        // Fall through to default is intentional here - no break is needed
+    default:
+        // Default case intentionally empty
+        break;
+    }
 
-	return x;
+    return x;
 }
